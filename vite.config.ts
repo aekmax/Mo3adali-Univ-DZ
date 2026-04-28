735db762b73def92f0792cdf3b9236337e84893b
aekmax <aekmax@hotmail.fr>
2026-04-28 14:37:18 +0100

    Merge remote changes

--- vite.config.ts
+++ vite.config.ts
@@ -17,8 +17,8 @@ export default defineConfig({
         display: "standalone",
         orientation: "portrait",
         icons: [
-          { src: "pwa-192.png", sizes: "192x192", type: "image/png" },
-          { src: "pwa-512.png", sizes: "512x512", type: "image/png", purpose: "any maskable" },
+          { src: "public/logo.webp", sizes: "512x512", type: "image/png/webp" },
+          { src: "public/logo.webp", sizes: "512x512", type: "image/png/webp", purpose: "any maskable" },
         ],
       },
       workbox: {
